695c103b53ef5fac180ddef85ba912d95af9480b
awlers <walidalaouimrani@gmail.com>
2021-11-30 19:39:30 +0100

    59

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({contact}) => {
 
+    const contactContext = useContext(ContactContext);
+    const {deleteContact, setCurrent, clearCurrent} = contactContext;
+
     const {id, name, email, phone, type} = contact; 
+
+    const onDelete = () => {
+        deleteContact(id);
+        clearCurrent();
+    }
 
     return (
         <div className="card bg-light">
@@ -31,8 +40,8 @@ const ContactItem = ({contact}) => {
 
             </ul>
             <p>
-                <button className="btn btn-dark btn-sm">Edit</button>
-                <button className="btn btn-danger btn-sm">Delete</button>
+                <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
+                <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
             </p>
             
         </div>
@@ -41,6 +50,6 @@ const ContactItem = ({contact}) => {
 
 ContactItem.propTypes = {
     contact: PropTypes.object.isRequired,
-    
+
 }
 export default ContactItem;
